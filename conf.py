c050bb9e0e34bc12d1f05739a875cc88264c94d6
Chris Holdgraf <choldgraf@gmail.com>
2021-09-10 11:18:36 -0700

    Update intersphinx links

--- conf.py
+++ conf.py
@@ -66,8 +66,8 @@ myst_enable_extensions = [
 ]
 myst_url_schemes = ["https", "http", "ftp", "mailto"]
 intersphinx_mapping = {
-    "pi": ("https://pilot.2i2c.org", None),
-    "ph": ("https://pilot-hubs.2i2c.org", None),
+    "pi": ("https://pilot.2i2c.org/en/latest", None),
+    "ph": ("https://pilot-hubs.2i2c.org/en/latest", None),
 }
 
 panels_add_bootstrap_css = False
