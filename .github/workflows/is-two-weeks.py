0c4bdec954711e16d4fe8a2716a0c614035a83a4
Chris Holdgraf <choldgraf@berkeley.edu>
2021-11-17 14:40:59 -0800

    Fixing weeks calculation

--- .github/workflows/is-two-weeks.py
+++ .github/workflows/is-two-weeks.py
@@ -7,13 +7,13 @@ from datetime import date
 # This is a Tuesday before a sprint meeting, so we'll calculate every-two-weeks relative to this
 start_date = date(2021,11,9)  
 difference_days = abs(date.today() - start_date).days
-
-# // makes result an integer (and rounds down)
-weeks = (difference_days // 7) + 1  # Add 1 because we are effectively 0-indexed
-is_two_weeks = (weeks % 2) == 0
+n_days_since_last_meeting = difference_days % 14  # Because we have a new meeting every 14 days.
+n_weeks_since_last_meeting = (n_days_since_last_meeting // 7) + 1
+is_two_weeks = (n_weeks_since_last_meeting % 2) == 0  # Add 1 because we are effectively 0-indexed
 
 # Store the output value for use later
 # See https://docs.github.com/en/actions/learn-github-actions/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
 # for more information
-print(f'::set-output name=N_WEEKS_SINCE::{weeks}')
+print(f'::set-output name=N_DAYS_SINCE::{n_days_since_last_meeting}')
+print(f'::set-output name=N_WEEKS_SINCE::{n_weeks_since_last_meeting}')
 print(f'::set-output name=IS_TWO_WEEKS::{is_two_weeks}')
