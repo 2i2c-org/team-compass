ec94ef7022ac559f68e9510aac828813365b5c82
Chris Holdgraf <choldgraf@berkeley.edu>
2021-11-17 14:17:39 -0800

    Minor updates to github action

--- .github/workflows/datetime.py
+++ .github/workflows/datetime.py
@@ -2,15 +2,12 @@
 from datetime import date
 import os
 
-#provide the 1st date in YYYY,MM,DD format
-start_date = date(2021,11,1)
-
-#getting the result, abs = absolute value
-#(date1-date2).days gives an integer number of dates
-days = abs(date.today() - start_date).days
+# This is a Tuesday before a sprint meeting, so we'll calculate every-two-weeks relative to this
+start_date = date(2021,11,9)  
+difference_days = abs(date.today() - start_date).days
 
 # // makes result an integer (and rounds down)
-weeks = days // 7
+weeks = difference_days // 7
 is_two_weeks = (weeks % 2) == 0
 
 # Store the variable value for use later
