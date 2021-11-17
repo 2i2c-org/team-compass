41f5c7fa529d84c8fc98db0a98f14db87afd0fc4
Chris Holdgraf <choldgraf@berkeley.edu>
2021-11-17 14:31:28 -0800

    Also list the number of weeks

--- .github/workflows/is-two-weeks.py
+++ .github/workflows/is-two-weeks.py
@@ -9,10 +9,11 @@ start_date = date(2021,11,9)
 difference_days = abs(date.today() - start_date).days
 
 # // makes result an integer (and rounds down)
-weeks = difference_days // 7
+weeks = (difference_days // 7) + 1  # Add 1 because we are effectively 0-indexed
 is_two_weeks = (weeks % 2) == 0
 
 # Store the output value for use later
 # See https://docs.github.com/en/actions/learn-github-actions/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
 # for more information
+print(f'::set-output name=N_WEEKS_SINCE::{weeks}')
 print(f'::set-output name=IS_TWO_WEEKS::{is_two_weeks}')
