acef83a6e33ca86c922cd24c840bd15153d35d4e
Chris Holdgraf <choldgraf@berkeley.edu>
2021-11-17 14:21:52 -0800

    Improve comments in GHA

--- .github/workflows/datetime.py
+++ .github/workflows/datetime.py
@@ -1,6 +1,8 @@
-#import date function from datetime module
+"""
+Calculate whether the number of weeks since a reference date is divisible by 2.
+And use GitHub Actions outputs to store the result for use later on in the action.
+"""
 from datetime import date
-import os
 
 # This is a Tuesday before a sprint meeting, so we'll calculate every-two-weeks relative to this
 start_date = date(2021,11,9)  
@@ -10,5 +12,7 @@ difference_days = abs(date.today() - start_date).days
 weeks = difference_days // 7
 is_two_weeks = (weeks % 2) == 0
 
-# Store the variable value for use later
+# Store the output value for use later
+# See https://docs.github.com/en/actions/learn-github-actions/workflow-commands-for-github-actions#using-workflow-commands-to-access-toolkit-functions
+# for more information
 print(f'::set-output name=IS_TWO_WEEKS::{is_two_weeks}')
