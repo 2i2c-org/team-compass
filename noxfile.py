bdd176c33c99180835bda046d34f11d10b129f9c
Chris Holdgraf <choldgraf@berkeley.edu>
2022-12-16 10:21:06 +0100

    Document our GitHub accounts (#593)

--- noxfile.py
+++ noxfile.py
@@ -7,23 +7,20 @@ build_command = ["-b", "html", ".", "_build/html"]
 
 @nox.session
 def docs(session):
+    """Build the documentation. Use `-- live` for a live server to preview changes."""
     session.install("-r", "requirements.txt")
-    cmd = ["sphinx-build"]
-    cmd.extend(build_command + session.posargs)
-    session.run(*cmd)
-
-
-@nox.session(name="docs-live")
-def docs_live(session):
-    session.install("-r", "requirements.txt")
-
-    AUTOBUILD_IGNORE = [
-        "_build",
-        "build_assets",
-        "tmp",
-    ]
-    cmd = ["sphinx-autobuild"]
-    for folder in AUTOBUILD_IGNORE:
-        cmd.extend(["--ignore", f"*/{folder}/*"])
+    if "live" in session.posargs:
+        session.posargs.pop(session.posargs.index("live"))
+        session.install("sphinx-autobuild")
+        AUTOBUILD_IGNORE = [
+            "_build",
+            "build_assets",
+            "tmp",
+        ]
+        cmd = ["sphinx-autobuild"]
+        for folder in AUTOBUILD_IGNORE:
+            cmd.extend(["--ignore", f"*/{folder}/*"])
+    else:
+        cmd = ["sphinx-build"]
     cmd.extend(build_command + session.posargs)
     session.run(*cmd)
