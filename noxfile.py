33e7ae6c135cf45c9a8e8ebd0a0d4b96f2840557
Chris Holdgraf <choldgraf@berkeley.edu>
2022-12-16 00:37:45 -0800

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -7,8 +7,7 @@ build_command = ["-b", "html", ".", "_build/html"]
 
 @nox.session
 def docs(session):
-    """Build the documentation. Use `-- live` to build documentation with a
-    live server to preview changes."""
+    """Build the documentation. Use `-- live` for a live server to preview changes."""
     session.install("-r", "requirements.txt")
     if "live" in session.posargs:
         session.posargs.pop(session.posargs.index("live"))
